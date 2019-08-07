7185c7c036d9b8983e442006070809db2a5a094c
kolesarj <45079995+kolesarj@users.noreply.github.com>
2019-08-07 14:38:40 -0400

    fixed issue with datastore initialization

--- ckan-base/setup/prerun.py
+++ ckan-base/setup/prerun.py
@@ -131,8 +131,7 @@ def init_datastore_db():
 
         perms_sql = datastore_perms.stdout.read()
         # Remove internal pg command as psycopg2 does not like it
-        perms_sql = perms_sql.replace('ckan@isb-postgresql-ckan-dev','ckan')
-        perms_sql = perms_sql.replace('datastore_ro@isb-postgresql-ckan-dev','datastore_ro')
+        perms_sql = perms_sql.replace('@isb-postgresql-ckan-dev','')
         cursor.execute(perms_sql)
         for notice in connection.notices:
             print notice
