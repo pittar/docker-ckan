031168ec430cb68cfff402803dc4fe796b429726
kolesarj <45079995+kolesarj@users.noreply.github.com>
2019-08-08 15:42:15 -0400

    Update prerun.py

--- ckan-base/setup/prerun.py
+++ ckan-base/setup/prerun.py
@@ -132,9 +132,9 @@ def init_datastore_db():
         # Remove internal pg command as psycopg2 does not like it
         perms_sql = re.sub('\\\\connect \"(.*)\"', '', perms_sql)
         # Strip the fully qualified Postgresql user name for database script"
-#        perms_sql = re.sub('(%40([^"]|"")*")', '"', perms_sql)
-        perms_sql = perms_sql.replace("ckan%40isb-postgresql-ckan-dev","ckan")
-        perms_sql = perms_sql.replace("datastore_ro%40isb-postgresql-ckan-dev","datastore_ro")
+        perms_sql = re.sub('(%40([^"]|"")*")/g', '"', perms_sql)
+#        perms_sql = perms_sql.replace("ckan%40isb-postgresql-ckan-dev","ckan")
+#        perms_sql = perms_sql.replace("datastore_ro%40isb-postgresql-ckan-dev","datastore_ro")
         print perms_sql
         cursor.execute(perms_sql)
         for notice in connection.notices:
@@ -210,4 +210,4 @@ if __name__ == '__main__':
         init_db()
         init_datastore_db()
         create_sysadmin()
-        init_organizations()
+#        init_organizations()
